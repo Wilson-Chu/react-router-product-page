bf84da774b2bdd6d6befb3bb845b464b3856c395
Wilson-Chu <wilson1.chu@gmail.com>
2022-08-04 13:14:24 -0400

    initial commit

---
+++ src/pages/Error.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <section className='section'>
+      <h2>Error 404</h2>
+      <p>Page Not Found</p>
+      <Link to="/">Go back to Home</Link>
+    </section>
+  );
+};
+export default Error;
